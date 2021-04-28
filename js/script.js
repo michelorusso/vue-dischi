3a5faf4a3778fca9aab0b33ac86f68b975a277e8
Michele Lorusso <kirito102095@hotmail.it>
2021-04-28 15:26:28 +0200

    Exercise complete (no bonus)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-
+// Attraverso una chiamata ajax all’API di boolean avremo a disposizione una decina di dischi musicali.
+// https://flynn.boolean.careers/exercises/api/array/music 
+//  Utilizzando vue, stampiamo a schermo una card per ogni album.
+// BONUS: Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+// BONUS 2: Ordinare i dischi per anno di uscita.
 var app = new Vue(
     {
     el: '#root',
     data: {  
-        emailArray: [],
+        songList: [],
     }, 
     methods: {
     },
     mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music ')
+            .then((response) => {
+                const result = response.data;
+
+                this.songList = result.response ;
+            });
     }
 });
